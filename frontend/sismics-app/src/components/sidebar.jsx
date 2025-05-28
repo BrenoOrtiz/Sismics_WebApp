76e43fada7acc54db8b17629c3f164ba07d71c41
Breno <breno.ortizdavid@outlook.com>
2025-05-27 22:44:58 -0300

    add/ pass seismicEvents data to sidebar

--- frontend/sismics-app/src/components/sidebar.jsx
+++ frontend/sismics-app/src/components/sidebar.jsx
@@ -2,14 +2,8 @@ import "./sideBar.css"
 import RealTime from "../components/realTime"
 import SearchBarSide from "./searchBarSide";
 
-export default function Sidebar() {
+export default function Sidebar({seismicEvents }) {
 
-    const seismicEvents = [
-        { id: 1, name: "Tokyo", magnitude: 5.2 },
-        { id: 2, name: "Nova Guinea", magnitude: 3.1 },
-        { id: 3, name: "Argentina", magnitude: 6.8 },
-        { id: 4, name: "Brazil", magnitude: 4.5 },
-    ]
     return (
         <div className="sidebarContainer">
             <SearchBarSide/>
