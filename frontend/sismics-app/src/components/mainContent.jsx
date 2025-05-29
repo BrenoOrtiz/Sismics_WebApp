45225d2f0dd1d1a0b0b024bd65b289613f5ef17a
Breno <breno.ortizdavid@outlook.com>
2025-05-28 23:45:16 -0300

    add/ focuspointData state for globe focus change

--- frontend/sismics-app/src/components/mainContent.jsx
+++ frontend/sismics-app/src/components/mainContent.jsx
@@ -5,20 +5,20 @@ import "./mainContent.css"
 import SearchBar from "./searchBar"
 import dynamic from 'next/dynamic';
 
-const GlobeComponent = dynamic(() => import("./globe"), { 
-    ssr: false, 
-    loading: () => <p>Carregando Globo...</p>
-});
+import { useState } from "react";
+
+const GlobeComponent = dynamic(() => import("./globe"), {ssr: false});
 
 export default function MainContent({seismicEvents}) {
     
+    const [focusPointsData, setFocusPointsData] = useState({ lat: 0, lng: 0, altitude: 2.5 });
 
     return (
         <div className="mainContentContainer">
             <h1 className="title">Monitoramento de Abalo Sísmico</h1>
             <p className="description">Acompanhe em tempo real os abalos sísmicos ao redor do mundo.</p>
-            <SearchBar />
-            <GlobeComponent seismicEvents={seismicEvents}/>
+            <SearchBar seismicEvents={seismicEvents} handleFocusPoints={setFocusPointsData} />
+            <GlobeComponent seismicEvents={seismicEvents} pointsData={focusPointsData} />
         </div>
     )
 }
