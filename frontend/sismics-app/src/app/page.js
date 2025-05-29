75679b9c86d47f2ee5f5ca13b5910cec14a71302
Breno <breno.ortizdavid@outlook.com>
2025-05-28 23:43:21 -0300

    add/ error treatment

--- frontend/sismics-app/src/app/page.js
+++ frontend/sismics-app/src/app/page.js
@@ -13,25 +13,34 @@ import MainContent from "../components/MainContent";
 export default function Home() {
   
   const [seismicEvents, setSeismicEvents] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     
     const fetchSeismicEvents = async () => {
-      const response = await axios.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson")
-      const events = response.data.features.map(element => ({
-        id: element.id,
-        magnitude: element.properties.mag,
-        place: element.properties.place,
-        time: new Date(element.properties.time),
-        lng: element.geometry.coordinates[0],
-        lat: element.geometry.coordinates[1],
-        alert: element.properties.alert,
-        depth: element.properties.dmin,
-        tsumnami: element.properties.tsunami,
-
-      }))
-
-      setSeismicEvents(events);
+      try {
+        const response = await axios.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson")
+        const events = response.data.features.map(element => ({
+          id: element.id,
+          magnitude: element.properties.mag,
+          place: element.properties.place,
+          time: new Date(element.properties.time),
+          lng: element.geometry.coordinates[0],
+          lat: element.geometry.coordinates[1],
+          alert: element.properties.alert,
+          depth: element.properties.dmin,
+          tsumnami: element.properties.tsunami,
+  
+        }))
+        
+        setSeismicEvents(events);
+        setLoading(false);
+      }
+      catch (error) {
+        console.log("Erro ao buscar eventos sísmicos:", error);
+        setSeismicEvents([])
+      }
+      
     };
 
     fetchSeismicEvents();
