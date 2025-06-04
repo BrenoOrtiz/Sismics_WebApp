643155e6797de88ca670cb7bf2ac6254524e2b9f
Breno <breno.ortizdavid@outlook.com>
2025-06-03 22:53:59 -0300

    add/ EarthquakeDetail component

---
+++ frontend/sismics-app/src/components/eathquakeDetail.jsx
@@ -0,0 +1,13 @@
+
+export default function EarthquakeDetail() { 
+
+    return (
+        <div>
+            {/* Container de detalhe */ }
+            <div>
+                <h2>Titulo</h2>
+                <p>detalhe</p>
+            </div>
+        </div>
+    )
+}
