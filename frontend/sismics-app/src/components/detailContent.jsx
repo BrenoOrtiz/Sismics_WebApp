86a2a81e438a4f765080fce6fbbd91eb737d081a
Breno <breno.ortizdavid@outlook.com>
2025-06-03 22:52:50 -0300

    add/ DetailContent component

---
+++ frontend/sismics-app/src/components/detailContent.jsx
@@ -0,0 +1,11 @@
+import "detailContent.css";
+
+export default function DetailContent() { 
+
+    return (
+        <div className="detail-content-container">
+            <h1 className="title">Detalhes de Terremoto</h1>
+            <p className="local">Local</p>
+        </div>
+    )
+}
