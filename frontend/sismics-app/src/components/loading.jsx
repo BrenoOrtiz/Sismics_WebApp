f3b552608b57f142ca11ea4836f8820ff88e597a
Breno <breno.ortizdavid@outlook.com>
2025-06-04 23:32:47 -0300

    add/ loading component

---
+++ frontend/sismics-app/src/components/loading.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import './loading.css';
+
+export default function Loading() { 
+  return (
+    <div className="loading-overlay"> 
+      <div className="loading-element"></div>
+    </div>
+  );
+};
