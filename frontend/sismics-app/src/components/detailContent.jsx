3e97a05108cc0201d03f29557640d08ce8f46eb4
Breno <breno.ortizdavid@outlook.com>
2025-06-04 23:32:01 -0300

    add/ conteudo da DetailContent component

--- frontend/sismics-app/src/components/detailContent.jsx
+++ frontend/sismics-app/src/components/detailContent.jsx
@@ -1,11 +1,14 @@
 import "detailContent.css";
 
-export default function DetailContent() { 
+import EarthquakeDetail from "./eathquakeDetail";
+
+export default function DetailContent({data}) { 
 
     return (
         <div className="detail-content-container">
             <h1 className="title">Detalhes de Terremoto</h1>
-            <p className="local">Local</p>
+            <p className="local">{data.place}</p>
+            <EarthquakeDetail data={data} />
         </div>
     )
 }
