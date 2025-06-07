5db7c84024bff57c8b48258565e34d049a91454a
Breno <breno.ortizdavid@outlook.com>
2025-06-07 18:24:37 -0300

    add/ RadiusData component rendering

--- frontend/sismics-app/src/components/detailContent.jsx
+++ frontend/sismics-app/src/components/detailContent.jsx
@@ -5,11 +5,14 @@ import RadiusData from "./radiusData";
 
 export default function DetailContent({data}) { 
 
+    const location = {"lat": data.lat, "lng": data.lng};
+
     return (
         <div className="detail-content-container">
             <h1 className="title">Detalhes de Terremoto</h1>
             <p className="local">{data.place}</p>
             <EarthquakeDetail data={data} />
+            <RadiusData location={location} />
         </div>
     )
 }
