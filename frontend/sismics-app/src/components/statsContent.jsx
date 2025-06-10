f7e801d4174f3c241c38666a0358e2f40ad1a820
Breno <breno.ortizdavid@outlook.com>
2025-06-09 21:43:29 -0300

    add/ statsContent component

---
+++ frontend/sismics-app/src/components/statsContent.jsx
@@ -0,0 +1,37 @@
+"use client"
+
+import "./statsContent.css"
+
+import FilterDate from "./filterDate"
+import StatsCard from "./statsCard"
+
+export default function StatsContent() {
+
+    return (
+
+        <div className="stats-content">
+            <h1 className="stats-title">Estatísticas</h1>
+            <p className="stats-description">Aqui você pode visualizar as estatísticas dos dados sísmicos.</p>
+
+            <FilterDate/>
+            <div className="stats-container">
+                <StatsCard
+                    title="Total de Sismos"
+                    value="1.234"
+                    description="Número total de sismos registrados."
+                />
+                <StatsCard
+                    title="Sismos por Magnitude"
+                    value="5.6"
+                    description="Magnitude média dos sismos registrados."
+                />
+                <StatsCard
+                    title="Sismos por Profundidade"
+                    value="10 km"
+                    description="Profundidade média dos sismos registrados."
+                />
+            </div>
+        </div>
+    )
+
+ }
