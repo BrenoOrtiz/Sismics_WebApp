6816ffe035e4eec2647c9a6e3417a5a30799e41e
Breno <breno.ortizdavid@outlook.com>
2025-06-09 21:44:11 -0300

    add/ statsCard.jsx component

---
+++ frontend/sismics-app/src/components/statsCard.jsx
@@ -0,0 +1,16 @@
+
+
+import "./statsCard.css"
+
+export default function StatsCard({ title, value, description }) {
+
+    return (
+        <div>
+            <div className="stats-card">
+                <h2 className="stats-card-title">{title}</h2>
+                <div className="stats-card-value">{value}</div>
+                {description && <p className="stats-card-description">{description}</p>}
+            </div>
+        </div>
+    )
+ }
