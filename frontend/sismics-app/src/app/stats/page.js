d86095bd96dd7b5cfdee6d639cfb380f2d32f495
Breno <breno.ortizdavid@outlook.com>
2025-06-09 21:43:41 -0300

    add/ stats page

---
+++ frontend/sismics-app/src/app/stats/page.js
@@ -0,0 +1,14 @@
+
+import NavBar from "@/components/nav"
+import StatsContent from "@/components/statsContent"
+
+
+export default function StatsPage() { 
+
+    return (
+        <div>
+            <NavBar/>
+            <StatsContent/>
+        </div>
+    )
+}
