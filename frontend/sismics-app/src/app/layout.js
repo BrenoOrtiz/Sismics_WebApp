ac5510e19f335c57d6af4280efbcc9cd88774bdf
Breno <breno.ortizdavid@outlook.com>
2025-06-12 00:16:46 -0300

    add/ suppressHydrationWarning

--- frontend/sismics-app/src/app/layout.js
+++ frontend/sismics-app/src/app/layout.js
@@ -1,10 +1,16 @@
 
 import "./globals.css";
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
   return (
-    <html>
+    <html suppressHydrationWarning>
       <body suppressHydrationWarning>
+        <Script
+          src="https://kit.fontawesome.com/d5fcfde473.js"
+          crossOrigin="anonymous"
+          strategy="beforeInteractive"
+        />
         {children}
       </body>
     </html>
