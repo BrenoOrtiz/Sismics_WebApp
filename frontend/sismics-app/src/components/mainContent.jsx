95b083df4d46ca226c7d7e35d7576acec443cc3a
Breno <breno.ortizdavid@outlook.com>
2025-06-12 00:19:34 -0300

    update/ api call para terremotos da ultima semana

--- frontend/sismics-app/src/components/mainContent.jsx
+++ frontend/sismics-app/src/components/mainContent.jsx
@@ -20,16 +20,15 @@ export default function MainContent() {
     const fetchSeismicEvents = async () => {
       setLoading(true); 
       try {
-        const response = await axios.get("http://localhost:5000/api/earthquake/data") 
-        const events = response.data.map(element => ({
-          id: element.code, 
-          magnitude: element.mag,
-          place: element.place,
-          time: new Date(element.time),
-          lng: element.longitude, 
-          lat: element.latitude,  
-          alert: element.alert,
-          tsunami: element.tsunami
+        const response = await axios.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson") 
+        const events = response.data.features.map(element => ({
+          id: element.id,
+          magnitude: element.properties.mag,
+          place: element.properties.place,
+          time: new Date(element.properties.time),
+          lng: element.geometry.coordinates[0],
+          lat: element.geometry.coordinates[1],  
+          alert: element.properties.alert,
     
         }))
         
